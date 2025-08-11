917f84122923421949eb09542764b1efbba73f44
elthik <mur@elth>
2025-08-12 01:38:23 +0500

    - Сортировка вставками - Google Test

---
+++ alg.cpp
@@ -0,0 +1,29 @@
+﻿// alg.cpp: определяет точку входа для приложения.
+//
+
+#include "alg.h"
+
+#include <vector>
+
+#include "sorting.h"
+
+using namespace std;
+
+template <typename T>
+void printVector(const std::vector<T>& vec) {
+	std::cout << "[ ";
+	for (const auto& item : vec) {
+		std::cout << item << " ";
+	}
+	std::cout << "]\n";
+}
+
+int main()
+{
+	std::vector<int> v{ 31, 41, 59, 26, 41, 58 };
+
+	printVector(insertion_sort(v, v.size()));
+	
+	cout << "Hello CMake." << endl;
+	return 0;
+}
